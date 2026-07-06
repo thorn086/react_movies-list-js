015866c23df277ef46e1c6241144faa4ca55d81e
Darren Rose <thorn086@gmail.com>
2026-07-05 19:57:03 -0500

    fix: updated map comment

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ import { MovieList } from './components/MovieList/MovieList';
 export const App = () => (
   <div className="page">
     <div className="page-content">
-      <MovieList movies={moviesFromServer} />
+      <div className="movies">
+        <MovieList movies={moviesFromServer} />
+      </div>
     </div>
     <div className="sidebar" data-cy="Sidebar">
       Sidebar will be here
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -3,10 +3,10 @@ import './MovieList.scss';
 
 export const MovieList = ({ movies }) => {
   return (
-    <div className="movies">
+    <>
       {movies.map(movie => (
         <MovieCard key={movie.imdbId} movie={movie} />
       ))}
-    </div>
+    </>
   );
 };
