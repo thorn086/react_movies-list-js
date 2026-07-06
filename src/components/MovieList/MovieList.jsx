929e3aac2da5a6780f422dd6a30b24bac6e67bc7
Darren Rose <thorn086@gmail.com>
2026-07-05 20:15:27 -0500

    fix: updated components

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -3,10 +3,10 @@ import './MovieList.scss';
 
 export const MovieList = ({ movies }) => {
   return (
-    <>
+    <div className="movies" data-cy="MoviesList">
       {movies.map(movie => (
         <MovieCard key={movie.imdbId} movie={movie} />
       ))}
-    </>
+    </div>
   );
 };
